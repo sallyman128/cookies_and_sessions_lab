a27d5173c5df111b8181ef1617d5f8e3f7f75537
Salmaan <salmaan.arif.ali@gmail.com>
2021-05-16 18:20:11 -0400

    commit tree 061510a12928ea052a6ea35327c1587de0e50b3e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root 'products#index'
+  get '/index', to: 'products#index'
+  post '/add', to: 'products#add'
 end
